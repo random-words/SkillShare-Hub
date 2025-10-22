762db593f26b50d3c8a430984e663425ae423166
random-words <153684721+random-words@users.noreply.github.com>
2025-10-22 23:07:39 +0300

    delete tailwind

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import "./styles/tailwind.css";
+import "./styles/global.css";
 import router from "./app/router.jsx";
 
 createRoot(document.getElementById("root")).render(
